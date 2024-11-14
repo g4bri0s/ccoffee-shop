5c7b6c81fb9d8109ae9d48c2e9f856ea178e6be4
g4bri0s <g.cristianods@gmail.com>
2024-11-13 23:19:06 -0300

    adding customer-order

---
+++ src/app/interfaces/registerOrder.ts
@@ -0,0 +1,3 @@
+export interface IRegisterOrder {
+    customerName: string;
+  }
--- src/app/services/customer-orders.service.ts
+++ src/app/services/customer-orders.service.ts
@@ -1,9 +1,64 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { IOrder } from 'src/app/interfaces/order';
+import { IRegisterOrder } from 'src/app/interfaces/registerOrder';
+import { EMPTY, Observable, map, of, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomerOrdersService {
+  getOrders = 'orders';
+  api = environment.api;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getOrder() {
+    return this.http.get<IOrder[]>(`${this.api}/${this.getOrders}`);
+  }
+
+  getOrderById(id: number) {
+    return this.http.get<IOrder>(`${this.api}/${this.getOrders}/${id}`);
+  }
+
+  existOrderById(id: number): Observable<boolean> {
+    return this.http
+      .get<IOrder>(`${this.api}/${this.getOrders}/${id}`)
+      .pipe(
+        map((response) => {
+          return response !== null;
+        }),
+        catchError((error) => {
+          if (error.status === 404) {
+            return of(false);
+          } else {
+            throw error;
+          }
+        }),
+        map((exists) => {
+          return exists as boolean;
+        })
+      );
+  }
+
+  registerOrder(order: IRegisterOrder): Observable<any> {
+    return this.http.post(`${this.api}/${this.getOrders}`, order).pipe(
+      tap(() => {
+        return of(null);
+      }),
+      catchError((error) => {
+        return throwError(error);
+      })
+    );
+  }
+
+  editOrder(id: number, order: IOrder) {
+    return this.http.put(`${this.api}/${this.getOrders}/${id}`, order);
+  }
+
+  deleteOrder(id: number) {
+    return this.http.delete(`${this.api}/${this.getOrders}/${id}`);
+  }
 }
