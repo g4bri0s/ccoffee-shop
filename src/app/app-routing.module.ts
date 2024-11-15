98234c95ad9b03e852f15fb2d590bd8c3f6ffd4f
g4bri0s <g.cristianods@gmail.com>
2024-11-14 22:56:00 -0300

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoffeesComponent } from './pages/coffee/coffees/coffees.component';
 import { RegisterCoffeeComponent } from './pages/coffee/register-coffee/register-coffee.component';
+import { EditCoffeeComponent } from './pages/coffee/edit-coffee/edit-coffee.component';
+import { CustomerOrdersComponent } from './pages/customer-order/customer-orders/customer-orders.component';
+import { RegisterCustomerOrderComponent } from './pages/customer-order/register-customer-order/register-customer-order.component';
+import { EditCustomerOrderComponent } from './pages/customer-order/edit-customer-order/edit-customer-order.component';
+import { ItemComponent } from './pages/item/itens/item.component';
 
 const routes: Routes = [
   { path: 'coffees', component: CoffeesComponent },
   { path: 'register-coffee', component: RegisterCoffeeComponent },
-  //adicionar outras rotas
+  { path: 'edit-coffee/:id', component: EditCoffeeComponent },
+  { path: 'customer-orders', component: CustomerOrdersComponent },
+  { path: 'register-customer-order', component: RegisterCustomerOrderComponent },
+  { path: 'edit-customer-order/:id', component: EditCustomerOrderComponent },
+  { path: 'itens', component: ItemComponent },
 ];
 
 @NgModule({
